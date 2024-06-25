42b41b5f99699e2c9fc962bce30a081174050216
Lets <ruvan123@naver.com>
2024-06-25 17:45:29 +0900

    Feat: Implement Reusable Protocol

---
+++ AlpakaTheater/AlpakaTheater/Protocols/Reusable.swift
@@ -0,0 +1,18 @@
+//
+//  Reusable.swift
+//  AlpakaTheater
+//
+//  Created by user on 6/25/24.
+//
+
+import UIKit
+
+protocol Reusable { }
+
+extension Reusable {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UICollectionViewCell: Reusable { }

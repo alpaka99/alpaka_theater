5a089903850f5825bcf02c92781bc6635fd63b41
Lets <ruvan123@naver.com>
2024-06-25 17:18:11 +0900

    Feat: Implement TMDBManager's Basic fetchData Feature

--- AlpakaTheater/AlpakaTheater/Managers/TMDBManager.swift
+++ AlpakaTheater/AlpakaTheater/Managers/TMDBManager.swift
@@ -4,6 +4,7 @@
 //
 //  Created by user on 6/25/24.
 //
+import Alamofire
 
 final class TMDBManager {
     static let shared = TMDBManager()
@@ -11,4 +12,28 @@ final class TMDBManager {
     private init () { }
     
     
+    func fetchTMDBData() {
+        let url = "https://api.themoviedb.org/3/trending/movie/day"
+        
+        let parameters: Parameters = [
+            "language" : "en-US"
+        ]
+        
+        let headers = TMDBAPIKey.headers
+        
+        AF.request(
+            url,
+            parameters: parameters,
+            headers: headers
+        )
+        .responseString { response in
+            switch response.result {
+            case .success(let value):
+                dump(value)
+            case .failure(let error):
+                print("ERROR")
+                print(error.localizedDescription)
+            }
+        }
+    }
 }

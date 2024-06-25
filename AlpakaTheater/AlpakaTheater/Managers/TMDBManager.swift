632f638b0acb9a0ccb98498f1032bdf1a224f23d
Lets <ruvan123@naver.com>
2024-06-25 17:07:37 +0900

    Feat: Create TMDB Manager

---
+++ AlpakaTheater/AlpakaTheater/Managers/TMDBManager.swift
@@ -0,0 +1,14 @@
+//
+//  TMDBManager.swift
+//  AlpakaTheater
+//
+//  Created by user on 6/25/24.
+//
+
+final class TMDBManager {
+    static let shared = TMDBManager()
+    
+    private init () { }
+    
+    
+}
